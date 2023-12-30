fe405c952d78b41c80b75ef1ccb33737020d5d72
Zunickx <Zunickx@gmail.com>
2023-12-30 22:38:21 +0300

    Create file playing-area.js

---
+++ src/js/playing-area/playing-area.js
@@ -0,0 +1,25 @@
+export default class PlayingArea { // определяем класс PlayingArea, который дальше будем экспортировать по умолчанию
+    constructor(element) {
+    this._element = element;
+    }
+  
+    randomMovingGoblin() {
+      const goblinImg = document.createElement('img'); // создадим новый элемент img
+      goblinImg.classList.add('playing-area__img'); // для него добавим класс playing-area__img
+      goblinImg.src = 'https://raw.githubusercontent.com/netology-code/ahj-homeworks/video/dom/pic/goblin.png'; // установим источник изображения с помощью свойства src
+  
+      const playingItemsCollection = document.querySelectorAll('.playing-area__item'); // найдем коллецию элементов с классом .playing-area__item
+  
+      for(const playingItem of playingItemsCollection) { // Если у элемента есть первый дочерний элемент, то он удаляется с помощью метода remove
+        if(playingItem.firstElementChild) {
+          playingItem.firstElementChild.remove();
+        }
+      }
+  
+      const randomPlayingItem = Math.floor(Math.random() * playingItemsCollection.length); // генерируем случайный элемент из коллекции playingItemsCollection, для того чтоб гоблин 
+      // появлялся в различных местах на игровом поле
+  
+      playingItemsCollection[randomPlayingItem].append(goblinImg); // отображаем гоблина на игровом поле, посредством добавления этого элемента в качестве последнего 
+      // дочернего в выбранной коллекции playingItemsCollection
+    }
+  }
